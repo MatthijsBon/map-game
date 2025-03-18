5105dd309e3a476e310416ce5086db72a0008948
Matthijs Bon <matthijsbon@vandebron.nl>
2025-03-18 08:45:15 +0100

    Upgrade tailwind version
    branch:

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function MapApp() {
 
   return (
     <div className="bg-grey-300 min-h-screen">
-      <header className="bg-grey-100 shadow-sm">
+      <header className="bg-grey-100 shadow-xs">
         <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
           <div className="flex items-center">
             <h1 className="text-2xl font-bold text-blue-900">
